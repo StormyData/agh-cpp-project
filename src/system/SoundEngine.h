440ee52e2b2dc9eca19357c9e3237c0325c1ee20
Tobiasz Szulc <szulc.tobiasz@gmail.com>
2022-06-08 10:10:51 +0200

    fixed music playback, added local font copy

--- src/system/SoundEngine.h
+++ src/system/SoundEngine.h
@@ -6,26 +6,17 @@
 #include "AssetLoader.h"
 #include <list>
 #include <utility>
-class CallOnLoopMusic: public sf::Music
-{
-public:
-    explicit CallOnLoopMusic(std::function<void()> on_loop_hook): on_loop(std::move(on_loop_hook)) {}
-    sf::Int64 onLoop() override {on_loop(); return -1;}
-
-private:
-    std::function<void()> on_loop;
-};
 
 class SoundEngine {
 public:
-    explicit SoundEngine(AssetLoader& assetLoader): assetLoader(assetLoader), music([this](){this->on_music_loop();}) {}
+    explicit SoundEngine(AssetLoader& assetLoader): assetLoader(assetLoader){}
     void play_sound(const std::string& name);
     void play_music(const std::string& name, bool restart = false);
     void update();
 private:
     std::list<sf::Sound> sounds;
     AssetLoader& assetLoader;
-    CallOnLoopMusic music;
+    sf::Music music;
     std::string currently_playing_music_pool;
     MusicProperties current_music_properties;
     size_t current_index;
